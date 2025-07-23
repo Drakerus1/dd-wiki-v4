acd4209455f70858dd65cde0dce68461287acc72
Darius <dariusphom@gmail.com>
2025-07-22 23:25:39 -0400

    Quartz sync: Jul 22, 2025, 11:25 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "drakerus1.github.io/dd-wiki-v4",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
